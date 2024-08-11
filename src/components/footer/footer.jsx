669f6dd42008089d74572e909285988e15388681
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-08-12 05:26:25 +0530

    frontend setup done

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default async function Footer(){
+    return(
+        <>
+            <div className="bg-white">
+                <div className="mx-96 py-20 bg-[#38434d] rounded-t-3xl">
+
+                </div>
+            </div>
+        </>
+    )
+}

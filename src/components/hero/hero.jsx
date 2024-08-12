c83bf441149268e7e03d9d396d162dc4f37ac33a
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-08-12 19:12:07 +0530

    hero section made responsive

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -13,8 +13,8 @@ export default async function Hero(){
                 <h1 className="text-xl lg:text-6xl font-bold text-white">SCALE WITH <span className="text-[#9ac34d]">FINANCE</span> & <span className="text-[#9ac34d]">ACCOUNTING</span></h1>
                 <h2 className="text-xs lg:text-2xl leading-5 lg:leading-9 text-white font-normal w-3/4 lg:w-2/3 pt-5 lg:pt-10">We help <span className="text-[#9ac34d]">businesses scale</span> by supporting your <span className="text-[#9ac34d]">Finance & Accounting</span> needs with <span className="text-[#9ac34d]">Tech & Expertise</span></h2>
                 <div className="py-5 lg:py-10">
-                    <Button size="lg" className="hidden lg:block xl:block 2xl:block text-white text-xs lg:text-base active:scale-75 transform duration-400">Book a free consultation</Button>
-                    <Button size="sm" className="lg:hidden xl:hidden 2xl:hidden text-white text-xs lg:text-base active:scale-75 transform duration-400">Book a free consultation</Button>
+                    <Button size="lg" className="hidden lg:block xl:block 2xl:block text-white text-base active:scale-75 transform duration-400">Book a free consultation</Button>
+                    <Button size="sm" className="lg:hidden xl:hidden 2xl:hidden text-white text-xs active:scale-75 transform duration-400">Book a free consultation</Button>
                 </div>
             </div>
         </>

50635d4f0fa0e5b37d60727425ece79fb1ad1640
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-08-15 18:58:04 +0530

    navbar updated

--- src/components/header/subcomponents/navbar.jsx
+++ src/components/header/subcomponents/navbar.jsx
@@ -1,15 +1,21 @@
+"use client"
 import React from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Navbar() {
+
+    const pathname = usePathname();
+    const isActive = (route) => pathname === route;
+
     return (
         <>
             <div className="hidden md:block lg:block ">
                 <div className="flex gap-10">
-                    <Link href="./" className="text-[#9ac34d] text-sm font-medium uppercase">Home</Link>
-                    <Link href="./about" className="text-[#9ac34d] text-sm font-medium uppercase">About</Link>
-                    <Link href="./" className="text-[#9ac34d] text-sm font-medium uppercase">Services</Link>
-                    <Link href="./" className="text-[#9ac34d] text-sm font-medium uppercase">contact</Link>
+                    <Link href="/" className={`text-[#9ac34d] text-sm font-medium uppercase ${isActive("/")?"border-b border-[#9ac34d] scale-125":"border-b-0 scale-100"} p-1`}>Home</Link>
+                    <Link href="/about" className={`text-[#9ac34d] text-sm font-medium uppercase ${isActive("/about")?"border-b border-[#9ac34d] scale-125":"border-b-0 scale-100"} p-1`}>About</Link>
+                    <Link href="/" className={`text-[#9ac34d] text-sm font-medium uppercase ${isActive("/")?"border-b border-[#9ac34d] scale-125":"border-b-0 scale-100"} p-1`}>Services</Link>
+                    <Link href="/" className={`text-[#9ac34d] text-sm font-medium uppercase ${isActive("/")?"border-b border-[#9ac34d] scale-125":"border-b-0 scale-100"} p-1`}>contact</Link>
                 </div>
             </div>
         </>

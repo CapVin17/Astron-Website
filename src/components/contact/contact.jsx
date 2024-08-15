91d1e715f54a727b0f3c349b03f2e6bdcaae9ec5
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-08-15 22:12:30 +0530

    contact page done

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import msg from "@/components/contact/assets/message.jpg"
+import { Input } from "@nextui-org/react";
+import { Textarea } from "@nextui-org/react";
+import { Button } from "../ui/button";
+
+export default function Contact() {
+    return (
+        <>
+            <div className="h-full bg-white py-20 px-10 lg:px-72">
+                <h1 className="text-[#38434d] text-5xl lg:text-6xl font-extrabold pb-10 uppercase">Get In Touch</h1>
+                <div className="flex flex-wrap-reverse items-center justify-center md:py-10 lg:py-10 xl:py-10 2xl:py-10">
+                    <div className="w-full h-full md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 flex flex-col items-start justify-center gap-5 pt-5">
+                        <h1 className="text-[#38434d] text-3xl font-bold text-start bg-white uppercase">Contact us</h1>
+                        <Input type="text" color="secondary" label="Name" variant="bordered" className="text-[#38434d]" />
+                        <Input type="email" color="secondary" label="Email *" variant="bordered" className="text-[#38434d]" required />
+                        <Textarea type="text" color="secondary" label="Message *" variant="bordered" className="text-[#38434d]" required />
+                        <Button size="lg" className="text-white text-base active:scale-75 transform duration-400">Send</Button>
+                    </div>
+                    <div className="">
+                        <Image src={msg} />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}

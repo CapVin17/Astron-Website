7f01b8540b0d81939f513429a63014202ee09c17
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-08-20 11:08:47 +0530

    navbar updated

--- src/components/header/subcomponents/navbar.jsx
+++ src/components/header/subcomponents/navbar.jsx
@@ -15,6 +15,8 @@ export default function Navbar() {
                     <Link href="/" className={`text-sm uppercase ${isActive("/")?"text-[#67ac4d] font-bold scale-125":"text-[#9ac34d] font-medium scale-100"} p-1`}>Home</Link>
                     <Link href="/about" className={`text-sm uppercase ${isActive("/about")?"text-[#67ac4d] font-bold scale-125":"text-[#9ac34d] font-medium scale-100"} p-1`}>About</Link>
                     <Link href="/service" className={`text-sm uppercase ${isActive("/service")?"text-[#67ac4d] font-bold scale-125":"text-[#9ac34d] font-medium scale-100"} p-1`}>Services</Link>
+                    <Link href="/calculator" className={`text-sm uppercase ${isActive("/calculator")?"text-[#67ac4d] font-bold scale-125":"text-[#9ac34d] font-medium scale-100"} p-1`}>Calculator</Link>
+                    <Link href="/blog" className={`text-sm uppercase ${isActive("/blog")?"text-[#67ac4d] font-bold scale-125":"text-[#9ac34d] font-medium scale-100"} p-1`}>Blog</Link>
                     <Link href="/contact" className={`text-sm uppercase ${isActive("/contact")?"text-[#67ac4d] font-bold scale-125":"text-[#9ac34d] font-medium scale-100"} p-1`}>contact</Link>
                 </div>
             </div>

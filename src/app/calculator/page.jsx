2f141a2a8379523d8640a20bc41fe6ffdd55fc2d
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-08-21 16:07:38 +0530

    calculator started

--- src/app/calculator/page.jsx
+++ src/app/calculator/page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Calculator from "@/components/calculator/calculator";
 
 export default function Page(){
     return(
         <>
-            
+            <Calculator/>
         </>
     )
 }
---
+++ src/components/calculator/calculator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default async function Calculator() {
+    return (
+        <>
+            <div className="h-full bg-white pt-20 px-10 lg:px-72">
+                <h1 className="text-[#38434d] text-5xl lg:text-6xl font-extrabold pb-10">Loan</h1>
+            </div>
+        </>
+    )
+}
